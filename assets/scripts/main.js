4f00d74c8661366898e86dcd2a1d12902d4d034a
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-02-06 15:48:19 +0700

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,22 @@
+function onSignIn(googleUser) {
+  const google_token = googleUser.getAuthResponse().id_token;
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/google-auth',
+    data: { google_token }
+  })
+    .done(success => {
+      localStorage.access_token = success.token
+      // other function
+    })
+    .fail(err => {
+      // if fail
+    })
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
