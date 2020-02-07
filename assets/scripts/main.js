e019e39229f4cec8fed5247f915b27ea32256443
baufakhran <baufakhran@gmail.com>
2020-02-07 10:47:21 +0700

    perbaikan

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -27,192 +27,27 @@ function showPublicHoliday() {
     method: "GET"
   })
     .done(function(holidayDate) {
+      console.log(holidayDate)
+      let arr = ['JAN', 'FEB','MAR', 'APR', 'MEI', 'JUN', 'JUL','AUG', 'SEP', 'OKT', 'NOV', 'DES']
       holidayDate.data.forEach(element => {
-        let month = element.date.slice(5,7)
+        let month = +element.date.slice(6,7)
         let tanggal = element.date.slice(8,10)
-        if (month === "01") {
-           $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>JAN</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "02") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>FEB</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "03") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>MAR</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "04") {
-          console.log(tanggal, 'APRIL')
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>APR</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "05") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>MAY</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "06") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>JUN</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "07") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>JUL</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "08") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>AUG</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "09") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>SEP</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "10") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>OCT</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "11") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>NOV</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        } else if (month === "12") {
-          $("#public-holiday #dating").append(`
-           <li>
-              <div class="time">
-                <h2>DES</h2>
-              </div> 
-              <div class="details">
-                <div class="date">
-                  <h2>${tanggal}</h2>
-                </div>  
-                <h3>${element.localName}</h3> 
-              </div>
-              <div style="clear: both;"> </div>
-            </li>
-          `)
-        }
-       
+        $("#public-holiday #dating").append(
+          `
+               <li>
+                  <div class="time">
+                    <h2>${arr[month-1]}</h2>
+                  </div> 
+                  <div class="details">
+                    <div class="date">
+                      <h2>${tanggal}</h2>
+                    </div>  
+                    <h3>${element.localName}</h3> 
+                  </div>
+                  <div style="clear: both;"> </div>
+                </li>
+          `
+        )
       });
       
     })
@@ -220,6 +55,9 @@ function showPublicHoliday() {
       console.log(err)
     })
 }
+
+
+
 
 $(document).ready(function() {
   showPublicHoliday()
